4a2cb101d9f636ea666be9860a4492a586b4a7c7
Islam Bahnas <islambahnas1@gmail.com>
2022-06-15 06:36:35 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "/users/:user_id/posts", to: "posts#index"
-  get "/user/:user_id/posts/:id", to: "posts#show"
-  resources :users, only: [:index, :show]
+  root "users#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
