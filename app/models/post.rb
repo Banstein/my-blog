f93865d23262b0137238b4954e7f975768afd4bc
Islam Bahnas <islambahnas1@gmail.com>
2022-06-22 00:47:45 +0200

    fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-  after_save :update_post_counter
+  after_save :post_counter
 
-  def update_post_counter
+  def post_counter
     author.increment!(:posts_counter)
   end
 
