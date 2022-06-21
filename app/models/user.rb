59112cc2ea6e25b36cf96e09f870a3cdb5f80f5b
Islam Bahnas <islambahnas1@gmail.com>
2022-06-21 12:22:27 +0200

    Add relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :likes
-  has_many :comments
-end
-
-class Post < ApplicationRecord
-  belongs_to :user
-  has_many :likes
-  has_many :comments
-end
-
-class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
-end
-
-class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
 end
