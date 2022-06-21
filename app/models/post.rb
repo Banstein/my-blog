50a9bbbbd6cba72c55fa16c69a31ee5a9377fae7
Islam Bahnas <islambahnas1@gmail.com>
2022-06-21 12:24:05 +0200

    Add counters

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  def recents_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
