ec4611b6e7b6f0e8cc896ea80c49429e92921c37
Islam Bahnas <islambahnas1@gmail.com>
2022-06-21 12:27:46 +0200

    Add recents

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+  after_save :update_post_counter
 
+  def update_post_counter
+    author.increment!(:posts_counter)
+  end
+  
   def recents_comments
     comments.order(created_at: :desc).limit(5)
   end
